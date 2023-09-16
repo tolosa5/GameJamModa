9a36b3acc31899bdacbe64c582fff9046e9fdd82
tolosa5 <diegorinconesteban@gmail.com>
2023-09-16 03:28:17 +0200

    prendas y parte del personaje

--- Assets/Scripts/ScriptsDIego/Player.cs
+++ Assets/Scripts/ScriptsDIego/Player.cs
@@ -8,16 +8,22 @@ public class Player : MonoBehaviour
     Rigidbody rb;
     NavMeshAgent agent;
 
+    public List<int> desiredClothesIds;
+    int interactedObjId;
+    GameObject pickedGO;
+    int inventory;
+
     float h, v;
     Camera cam;
     GameObject camGO;
-    [SerializeField] float speed;
     Vector3 movDir;
 
     Vector3 targetPosition;
 
+    [SerializeField] string[] tags;
+    [SerializeField] LayerMask isInteractable;
 
-    enum States {aa, ee};
+    enum States {Normal, Holding};
     States currentState;
 
     private void Start()
@@ -34,11 +40,28 @@ public class Player : MonoBehaviour
         Inputs();
         Debug.Log(agent.isStopped);
         Debug.Log(targetPosition);
+
+        switch (currentState)
+        {
+            default:
+            case States.Normal:
+
+                break;
+            case States.Holding:
+
+                break;
+        }
     }
-    private void FixedUpdate()
+
+    private void OnTriggerEnter(Collider other)
     {
-        //rb.AddForce(movDir);
+        if (other.gameObject.layer == 6)
+        {
+            pickedGO = other.gameObject;
+            Interact(pickedGO.tag);
+        }
     }
+
     void Inputs()
     {
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(cam.ScreenPointToRay
@@ -50,18 +73,100 @@ public class Player : MonoBehaviour
         //movDir = new Vector3(h, v, 0).normalized * speed;
     }
 
+    void ShowInteractMessage(int i)
+    {
+        switch (i)
+        {
+            default:
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
+        //mostrar mensaje de interaccion
+    }
+
+    void Interact(string objTag)
+    {
+        if (objTag == tags[0])
+        {
+            ShowInteractMessage(0);
+        }
+        else if (objTag == tags[1])
+        {
+            ShowInteractMessage(1);
+        }
+        else if (objTag == tags[2])
+        {
+            ShowInteractMessage(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log(objTag);
+            if (objTag == tags[0] && currentState != States.Holding)
+            {
+                PickUp();
+                Clothes clothScr = pickedGO.GetComponent<Clothes>();
+                inventory = clothScr.GiveId();
+            }
+            else if (objTag == tags[1] && currentState == States.Holding)
+            {
+                FillBag();
+            }
+            else if (objTag == tags[2] && currentState == States.Holding)
+            {
+                DropToBasket();
+            }
+        }
+    }
+
     void PickUp()
     {
-
+        if (currentState == States.Normal)
+        {
+            currentState = States.Holding;
+        }
+        else
+        {
+            Debug.Log("manos llenas rey");
+        }
     }
 
-    void GiveObject()
+    void FillBag()
     {
-
+        for (int i = 0; i < desiredClothesIds.Count; i++)
+        {
+            if (desiredClothesIds[i] == inventory)
+            {
+                RightPlacement(i);
+            }
+            else
+            {
+                Punishment();
+            }
+            
+        }
     }
 
-    void Drop()
+    void Punishment()
     {
+        //cliente pierde paciencia, devolver prenda a cesta
+    }
 
+    void RightPlacement(int i)
+    {
+        desiredClothesIds.RemoveAt(i);
+        //algun feedback positivo
+    }
+
+    void DropToBasket()
+    {
+        inventory = 0;
     }
 }
