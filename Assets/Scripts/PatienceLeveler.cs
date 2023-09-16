3b8e3dd8b6844bbcb49c1b5bd470e406516b2776
ainhoaparra <correoelectronico57@gmail.com>
2023-09-16 16:21:15 +0200

    slider gradient

---
+++ Assets/Scripts/PatienceLeveler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PatienceLeveler : MonoBehaviour
+{
+    public Canvas Canvas2D;
+    public Image speechBubble;
+    [SerializeField] private float patienceTimer = 0.25f;
+    [SerializeField] private Gradient patienceGradient;
+    private float client = 1f;
+    private Color newPatienceColor;
+    private Coroutine drainPatienceCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speechBubble = GetComponent<Image>();
+        //see patience color
+        speechBubble.color = patienceGradient.Evaluate(client);
+        CheckPatienceAmount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePatience(float maxPatience, float currentPatience)
+    {
+        client = currentPatience / maxPatience;
+        drainPatienceCoroutine = StartCoroutine(DrainPatience());
+        CheckPatienceAmount();
+    }
+
+    private IEnumerator DrainPatience()
+    {
+        float fillAmount = speechBubble.fillAmount;
+        Color currentColor = speechBubble.color;
+        float elapsedTime = 0f;
+        while (elapsedTime < patienceTimer)
+        {
+            elapsedTime += Time.deltaTime;
+            //lerp the fill amount
+            speechBubble.fillAmount = Mathf.Lerp(fillAmount, client, (elapsedTime / patienceTimer));
+
+            yield return null;
+        }
+    }
+    private void CheckPatienceAmount()
+    {
+        newPatienceColor = patienceGradient.Evaluate(client);
+    }
+}
