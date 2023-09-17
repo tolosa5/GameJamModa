96b86dcb32d1e55bbf6ccadfdb3eb06f89be93bb
tolosa5 <diegorinconesteban@gmail.com>
2023-09-17 11:17:07 +0200

    manager actualizaciones

--- Assets/Scripts/ScriptsDIego/GameManager.cs
+++ Assets/Scripts/ScriptsDIego/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] float globalTimeLeft;
     [HideInInspector] public int clients;
     [HideInInspector] public float money;
+    float neededMoney;
     [SerializeField] float baseMoneyGains;
     [SerializeField] Image fillerImage;
 
@@ -21,7 +22,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] Text message;
     [HideInInspector] public GameObject messageGO;
-    [SerializeField] GameObject endDayPanel;
+    [SerializeField] GameObject endDayPanel, congratsPanel, gameOverPanel;
 
     [SerializeField] GameObject[] speechBubbles;
     [SerializeField] Sprite[] emojis;
@@ -32,6 +33,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] string[] messagesTexts;
 
     [SerializeField] List<Transform> placeHoldersPositions;
+    [SerializeField] GameObject[] clothes;
 
     [SerializeField] TextMeshProUGUI coins, day;
     [SerializeField] Image timerFiller;
@@ -67,6 +69,7 @@ public class GameManager : MonoBehaviour
         if (globalTimeLeft >= 0)
         {
             globalTimeLeft -= Time.deltaTime;
+            timerFiller.fillAmount = globalTimeLeft;
         }
         if (globalTimeLeft <= 0)
         {
@@ -88,7 +91,12 @@ public class GameManager : MonoBehaviour
 
     void ClothesOrganizer()
     {
-
+        for (int i = 0; i < clothes.Length; i++)
+        {
+            int rand = Random.Range(0, placeHoldersPositions.Count);
+            clothes[i].transform.position = placeHoldersPositions[rand].position;
+            placeHoldersPositions.RemoveAt(rand);
+        }
     }
 
     public void AskForBasket()
@@ -116,24 +124,45 @@ public class GameManager : MonoBehaviour
 
             float clientMoney = baseMoneyGains *= moneyMultiplier;
             money += clientMoney;
+
         }
     }
 
     void EndDay()
     {
         ActivateText(endDayPanel);
-        currentDay++;
+        EndDayPanelConfig();
     }
 
     void EndDayPanelConfig()
     {
-
+        if (currentDay == 1) congratsPanel.SetActive(true);
+        else
+        {
+            if (money <= neededMoney) gameOverPanel.SetActive(true);
+            else
+            {
+                congratsPanel.SetActive(true);
+            }
+        }
     }
+
+
+
 
     //se llama por UI
     public void NextDay()
     {
         globalTimeLeft = totalTime;
         DesactivateText(endDayPanel);
+        currentDay++;
+    }
+
+    //se llama por UI
+    public void Reestart()
+    {
+        globalTimeLeft = totalTime;
+        DesactivateText(endDayPanel);
+        currentDay = 1;
     }
 }
