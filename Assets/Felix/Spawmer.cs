d9c8f381285b134a767ca3778661e8eafdf6b024
Demiseman <felixluisvb@gmail.com>
2023-09-17 11:15:58 +0200

    Update Spawmer.cs

--- Assets/Felix/Spawmer.cs
+++ Assets/Felix/Spawmer.cs
@@ -6,49 +6,73 @@ public class Spawmer : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint; // Posición inicial.
     [SerializeField] private GameObject objectPrefab; // Prefab del objeto a spawnear.
-    [SerializeField] private int maxObjectsToSpawn = 5; // Número límite de personajes.
-    [SerializeField] private float spawnInterval = 3.0f; // Intervalo de tiempo entre cada spawneo.
+    [SerializeField] private int maxObjectsToSpawn = 8; // Número límite de personajes.
+    [SerializeField] private float spawnInterval = 1.0f; // Intervalo de tiempo entre cada spawneo.
+    public bool IsBuying = false;
 
-    public List<GameObject> spawnedClients = new List<GameObject>();
-    private float lastSpawnTime;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
 
-    public static Spawmer instance;
+    private static Spawmer instance;
+
+    // Propiedad estática para acceder a la instancia única.
+    public static Spawmer Instance
+    {
+        get { return instance; }
+    }
+
 
     private void Awake()
     {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-        }
+        if (instance != )
     }
 
     private void Start()
     {
-        lastSpawnTime = Time.time;
+        StartCoroutine(SpawnNPCs());
     }
 
-    private void Update()
+    private IEnumerator SpawnNPCs()
     {
-        //comprueba si se puede spawnear
-        if (spawnedClients.Count < maxObjectsToSpawn && Time.
-            time - lastSpawnTime >= spawnInterval)
+        while (true)
         {
-            SpawnObject();
+            if (spawnedObjects.Count < maxObjectsToSpawn)
+            {
+                GameObject newNPC = Instantiate(objectPrefab, spawnPoint.position, Quaternion.identity);
+                newNPC.transform.SetParent(transform); // Hacer al nuevo NPC hijo del Spawner.
+
+                spawnedObjects.Add(newNPC);
+
+                // Comprueba si se alcanzó el límite de NPCs y si IsBuying es falso.
+                if (spawnedObjects.Count == maxObjectsToSpawn && !IsBuying)
+                {
+                    // Selecciona aleatoriamente un NPC para comprar.
+                    int randomIndex = Random.Range(0, spawnedObjects.Count);
+                    GameObject npcToBuy = spawnedObjects[randomIndex];
+                    Debug.Log("Se ha seleccionado comprar: " + npcToBuy.name);
+
+                    // Activa la variable IsBuying.
+                    IsBuying = true;
+                }
+            }
+
+            // Espera al siguiente intervalo antes de intentar spawnear otro NPC.
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 
-    private void SpawnObject()
+    // Llamado cuando un NPC es destruido CONTAINS de la lista array que no existe
+    public void OnNPCDestroyed(GameObject npc)
     {
-        // Crea una nueva instancia del objeto y añade a lista
-        GameObject newObject = Instantiate(objectPrefab, spawnPoint.position, Quaternion.identity);
-        spawnedClients.Add(newObject);
+        if (spawnedObjects.Contains(npc))
+        {
+            spawnedObjects.Remove(npc);
 
-        // Actualiza el tiempo del último spawneo///
-        lastSpawnTime = Time.time;
+            // Comprueba si se ha reducido el número de NPCs interados en escena.
+            if (spawnedObjects.Count < maxObjectsToSpawn)
+            {
+                Debug.Log("Se ha reducido el número de NPCs interados. Spawneará nuevos NPCs.");
+            }
+        }
     }
 }
 
